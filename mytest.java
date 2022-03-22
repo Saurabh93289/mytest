35fc44e16326df937f377519307184cb2a26f80c
saurabhkumar93289 <saurabhkumar93289@gmail.com>
2022-03-22 12:54:47 +0530

    Added all files

--- mytest.java
+++ mytest.java
@@ -1,8 +1,5 @@
 public class mytest{
 public static void main(String args[]){
-System.out.println("Hello World!");
-System.out.println("Hello Java!");
-System.out.println("Hello Saurabh!");
-System.out.println("Hello Programming!");
+System.out.println("Hello mytest");
 }
 }
